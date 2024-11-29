b194e944aca55fa5c964c7da0d53648c2279be19
manvika-sahu <manvikasahu@dwao.in>
2024-11-29 22:47:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ document.getElementById('signup-form')?.addEventListener('submit', function(e) {
 });
 
 // This part is for any additional features you may want to implement, like dynamically loading content, etc.
+// Track external link clicks
+document.querySelectorAll('a[target="_blank"]').forEach(link => {
+    link.addEventListener('click', function () {
+        const url = link.href;
+
+        // Push event to the dataLayer
+        window.dataLayer.push({
+            event: 'externalLinkClick', // Custom event name
+            linkUrl: url               // URL of the clicked link
+        });
+
+        console.log('Tracked external link:', url); // For debugging
+    });
+});
